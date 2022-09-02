0094ba204a7fccb736b71c6263fb1151150d940b
Jeff Allen <jeffallen89@gmail.com>
2022-09-02 11:48:34 -0400

    adding some text, formatting page

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
 	import { onMount } from 'svelte'
 	import mapboxgl from "mapbox-gl";
-	// import 'mapbox-gl/dist/mapbox-gl.css';
 
 	mapboxgl.accessToken = 'pk.eyJ1Ijoic2Nob29sb2ZjaXRpZXMiLCJhIjoiY2w3aml0dHdlMHlpazNwbWh0em4xOHNlaCJ9.fXNtPGq0DqYiFvPH6p4fjQ';
 
 	let map;
+	let city = "Toronto"
 	
 	onMount(() => {
 		map = new mapboxgl.Map({
@@ -23,8 +23,9 @@
 		map.addControl(new mapboxgl.FullscreenControl(), 'top-right');
 	});
 
-	function mapZoom() {
-		map.zoomTo(12)
+	function mapPan(city) {
+		console.log(city)
+		map.zoomTo(10)
 	}
 
 </script>
@@ -37,22 +38,38 @@
 	<link href='https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css' rel='stylesheet' />
 </svelte:head>
 
-<div id="title">
+<main>
 
-	
+<div id="title">
+	<h1>25 Years of Population Growth & Decline in Canadian Cities</h1>
+	<p>Visualizing neighbourhood change from 1996 to 2021</p>
 </div>
 
 <div id="text">
 	<p>
-		Canadian urban regions have undergone substantial change over the the past several decades. In the map below we visualize by neighbourhood (represented by census tracts) whether the population has grown (QQQ) or declined (QQQ) between 1996 and 2021. Despite the total population across the 33 urban regions increasing by QQQQQ, QQQQ% of neighbourhoods witnessed population loss. A general trend is sprawling population growth in the suburban fringes and concentrated development in central nodes, with population decline occurring in many older pre-1996 residential neighbourhoods, but this isn't evident everywhere. Explore the map below to see more.
+		Canadian urban regions have undergone substantial changes over the the past several decades. In the maps below we visualize by neighbourhood (represented by census tracts) whether the population has grown (QQQ) or declined (QQQ) between 1996 and 2021. The total population across the 3X urban regions increased from X to Y, but this growth was uneven, with QQQQ% of neighbourhoods experiencing population loss. 
+	</p>
+</div>
+<div id="text">
+	<p>	
+		The maps show a general trend of population growth both in the suburban fringes and concentrated in some downtowns, with population decline occurring in many older pre-1996 residential neighbourhoods. But these trends aren't evident everywhere. Click on a city to zoom to it.
 	</p>
 </div>
 
-<div id="zoom">
-	<button on:click={mapZoom}>
-		click
-	</button>
+<div id="text">
+	<p>
+		<span id="cityZoom" on:click={mapPan}>Victoria</span> |
+		<span id="cityZoom" on:click={mapPan}>Vancouver</span> | 
+		<span id="cityZoom" on:click={mapPan}>Kamploops</span> |
+		<span id="cityZoom" on:click={mapPan}>Kelowna</span>
+
+	</p>
 </div>
+
+</main>
+
+
+
 
 <div id="map"></div>
 
@@ -73,15 +90,48 @@
 		background-color: #fff;
 	}
 
-	#text {
+	main {
 		margin: auto;
-		width: 600px;
+		max-width: 600px;
+	}
+
+	#title {
+		max-width: 600px;
+		margin-left: 20px;
+		margin-right: 20px;
+	}
+
+	#title h1 {
+		font-family: TradeGothicBold, sans-serif;
+		font-size: 28px;
+		color: #1E3765
+	}
+
+	#title p {
+		font-family: 'Roboto', sans-serif;
+		font-size: 15px;
+		color: #1E3765
+	}
+
+	#text {
+		max-width: 600px;
 		line-height: 1.5;
-		
+		border-left: solid 1px grey;
+		padding-left: 20px;
+		margin-left: 20px;
+		margin-right: 20px;
+		color: #454545
 	}
 
 	#zoom {
 		height: 50px;
+	}
+
+	#cityZoom {
+		color: #007FA3
+	}
+	#cityZoom:hover {
+		cursor: pointer;
 	}
 
 	#map {
